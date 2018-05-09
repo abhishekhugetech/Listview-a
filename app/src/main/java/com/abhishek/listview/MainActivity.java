237073de8e44260060f4a2b57cb1bf2ca1c982c8
hugetechabhishek <getexplanation@gmail.com>
2018-05-09 17:29:25 +0530

    Comments have been Added to the Java File

--- app/src/main/java/com/abhishek/listview/MainActivity.java
+++ app/src/main/java/com/abhishek/listview/MainActivity.java
@@ -18,10 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Selecting the ListView
         ListView myList = findViewById(R.id.listView);
+        // Creating  a new ArrayList and using asList to Add Data to the List
         final ArrayList<String> myFriendArrayList = new ArrayList<String>(asList("Alaric","Ian","Paul","John","Elijah","Klaus","Michael","Elena","Bonny","Enzo","Rebeka"));
+        // Creating an ArrayAdapter to Convert arrayList to a form in which we can add that arrayList to List View
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,myFriendArrayList);
+        // Setting Adapter for the List view to get the  List from
         myList.setAdapter(arrayAdapter);
+        //Setting an onclick Listener to the List Item
         myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
